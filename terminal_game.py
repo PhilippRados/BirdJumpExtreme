f375430d71ad4d8d777b5f9352a6ddd6c7667113
PythonPhil-Crack <phil.black@gmx.net>
2020-10-16 12:10:12 +0300

    smooth env movement and new platform mechanic

--- terminal_game.py
+++ terminal_game.py
@@ -1,5 +1,7 @@
 import curses
 import random
+import time
+from threading import Thread
 
 # initialize application
 stdscr = curses.initscr()
@@ -10,7 +12,7 @@ curses.curs_set(0)
 
 init_h,init_w = stdscr.getmaxyx()
 min_width = 90
-menu = ['PLAY', 'Scoreboard', 'Exit']
+menu = ['PLAY', 'Choose Character','Scoreboard', 'Exit']
 
 curses.start_color()
 curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_YELLOW)
@@ -35,28 +37,32 @@ def check_for_resize():
         too_large = False if resize_w < min_width else True
         stdscr.refresh() 
 
-def create_env(h,w, platform_anz=15):
-    number_of_platforms = (h // 2)-3
-    platform_pos = [(0,0),(h-5, random.randint(1,w-5))]
+def create_env(h,w):
+    number_of_platforms = (h // 1)-5
+    platform_pos = [(0,0),(h-1, random.randint(1,w-5))]
     for _ in range(number_of_platforms):
-        platform_pos.append((platform_pos[-1][0]-2, random.randint(1,w-5)))
+        platform_pos.append((platform_pos[-1][0]-1, random.randint(1,w-5)))
     del platform_pos[0]
     return platform_pos
-    #return [(random.randint(1,h-5), random.randint(1,w-5)) for i in range(platform_anz)]
 
 
 def print_env(env,counter):
-    for y,x in env:
+    loop_env = env
+    number_of_exceeded_platforms = 0
+    for y,x in loop_env:
         y += counter
         stdscr.addstr(init_h-1,x,"    ")
         if y > 0 and y < init_h:
             stdscr.addstr(y,x,"=====")
-            stdscr.addstr(y-1,x,"    ")
-        else:
+            stdscr.addstr(y-1,x,"     ")
+        elif y > init_h:
+            number_of_exceeded_platforms += 1
             y += counter
-            y_sorted_by_height = sorted(env, key=lambda tup: tup[0])
+            y_sorted_by_height = sorted(loop_env, key=lambda tup: tup[0])
             env.remove(y_sorted_by_height[-1])
-            env.append((1-counter, random.randint(1,init_w-5))) 
+            env.append(((3-counter) + number_of_exceeded_platforms, random.randint(1,init_w-5)))
+        else:
+           y += 1
 
 
 def move_left(current_x, current_y):
@@ -86,25 +92,50 @@ def play(resize_w, resize_h):
         stdscr.addstr(0,0,f"Score: {score}")
         
         stdscr.refresh()
-        if current_y < resize_h:
+        if current_y < resize_h and current_y > 0:
             stdscr.addstr(current_y,current_x,"o/00\o")
-        else:
+        elif current_y > resize_h:
             playing = False
 
-
+        #when platform hit
         if stdscr.instr(current_y+1, current_x,1) == b"=": #maybe use inwstr
-            current_y -= 8
             plateau_height[0] = plateau_height[1]
             plateau_height[1] = current_y
-            # maybe put a timeout here so that the player goes down slower
+
+            #when new platform is higher
+            if plateau_height[1] < plateau_height[0]:
+                diff = plateau_height[0] - plateau_height[1]
+                for i in range(8):
+                    time.sleep(0.01)
+                    stdscr.clear()
+                    if i <= diff:
+                        counter += 1
+                    current_y -= 1
+                    if current_y < resize_h and current_y > 0:
+                        stdscr.addstr(current_y,current_x,"o/00\o")
+                    elif current_y > resize_h:
+                        playing = False
+                    print_env(env,counter)
+                    stdscr.refresh()
+
+            #if new platform isnt higher than before
+            else:
+                for _ in range(8):
+                    stdscr.clear()
+                    print_env(env,counter)
+                    time.sleep(0.01)
+                    current_y -= 1
+                    if current_y < resize_h and current_y > 0:
+                        stdscr.addstr(current_y,current_x,"o/00\o")
+                    elif current_y > resize_h:
+                        playing = False
+                    stdscr.refresh()
+
         else:
             current_y += 1
         
-        if plateau_height[1] < plateau_height[0]:
-            counter += plateau_height[0] - plateau_height[1]
-            plateau_height[0] = current_y
 
-        stdscr.timeout(100)
+        stdscr.timeout(70)
         inp = stdscr.getch()
         if inp == curses.KEY_LEFT and current_x > 1:
             current_x -= 3 
@@ -116,8 +147,6 @@ def play(resize_w, resize_h):
     stdscr.clear()
     stdscr.addstr(resize_h//2,resize_w//2 - 10, f"Game over. Your score was {score}")
     stdscr.refresh()
-        
-
         
 
 def main(stdscr):
@@ -135,13 +164,19 @@ def main(stdscr):
         if key == curses.KEY_UP and current_row > 0:
             current_row -= 1
             print_menu(current_row)
-        elif key == curses.KEY_DOWN and current_row < 2:
+        elif key == curses.KEY_DOWN and current_row < 3:
             current_row += 1
             print_menu(current_row)
     
         #EXIT Button
-        if key == 10 and current_row == 2:
+        if key == 10 and current_row == 3:
             press_exit()
+            game = False
+        
+        if key == 10 and current_row == 1:
+            stdscr.clear()
+            stdscr.addstr(30,20,"Choose your character")
+            stdscr.refresh()
             game = False
 
         #PLAY Button
@@ -150,7 +185,7 @@ def main(stdscr):
             play(resize_w, resize_h)
             break
         
-        elif key == 10 and current_row == 1:
+        elif key == 10 and current_row == 2:
             stdscr.clear()
             question = "Work in progress..."
             stdscr.addstr(init_h//2,(init_w//2)-len(question)//2, question)
