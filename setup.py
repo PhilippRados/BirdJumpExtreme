dde1015df5354b051ebcf2ce42e40cae832b28b4
PythonPhil-Crack <phil.black@gmx.net>
2020-11-18 15:46:34 +0100

    corrected spelling mistake

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independant",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
