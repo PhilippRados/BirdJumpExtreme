4739312a5bdb7b854d0fa03d7999dc67e6d03a9a
PythonPhil-Crack <phil.black@gmx.net>
2020-11-18 15:35:07 +0100

    added setup.py and LICENSE.txt

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md","r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="BirdJumpExtreme",
+    version="0.0.1",
+    author="Philipp Rados",
+    author_email="phil.rados@gmail.com",
+    description="A fun platform-jumping game for your terminal",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url="https://github.com/PhilippRados/BirdJumpExtreme",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independant",
+    ],
+    python_requires='>=3.6',
+)
